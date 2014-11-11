789ad912fd6e970354ba1817fad5bc90f6333439
heabarnette <heather.e.adams@gmail.com>
2014-11-10 19:03:08 -0500

    initial

---
+++ js/app.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+
+// enter instead of button push
+//	$('#textenter').keyup(function(event) {
+//		if(event.keyCode == 13){
+//			event.preventDefault();
+//			$('#enter').click();
+//	};
+//	})
+	$('#enter').click(function (e) {
+		e.preventDefault();
+		$('#textenter').submit();
+	});
+// alerts
+//	$('#enter').click(function() {
+//	alert("The button was clicked");
+//	});
+//	$(':text').keyup(function() {
+//		alert("text was entered");
+	});
+// add to list with button
+	$('#textenter').submit(function(e) {
+		e.preventDefault();
+		additem();
+	});
+
+	var additem = function(){
+		var add = $('#textenter').val();
+		var newitem = $('<li class=get"></li><button class="check"></button><span>' + 
+add + '</span><img class="remove" src='images/Button-Delete-icon.png'>');
+		$('#list').append(newitem);
+		$('textenter').val('');
+	}
+
+//		var add = document.getElementById('textenter');
+//		if(newitem.length < 1)
+//			!$.trim($('#textenter').val())) {
+//			{alert('Add item');
+//		} else {
+		
+//		document.getElementById('textenter').value('');
+//		};
+//	})
+//
+// check off and uncheck items
+	$('.check').on('click', function () {
+	$(this).closest('li').toggleClass('done');
+	})
+// remove items
+	$('.remove').on('click', function() {
+		$(this).closest('li').remove();
+	})
+// hover makes delete button appear
+	$('#list').on('mousenter', 'li', function() {
+		$(this).children('.remove').toggleClass('show');
+	});
+	$('#list').on('mouseleave', 'li', function() {
+		$(this).children('.remove').toggleClass('show');
+	});
+
+// sort items
+	$('#list').sortable({axis: "y"});
+});
